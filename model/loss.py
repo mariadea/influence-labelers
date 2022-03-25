af28145edb52e2c57b9a468d5e9e24cc4dd73832
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2022-03-25 18:06:16 +0000

    L1 regularization

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,14 @@
+import torch
 import torch.nn as nn
 
-def compute_loss(model, x, y):
+def compute_loss(model, x, y, lbd = 0.001):
     pred = model(x).view(-1)
-    return nn.BCELoss()(pred, y)
+
+    # L2 regularization to avoid colineartity of the hessian
+    regularizer, params = 0, 0
+    for param in model.parameters():
+        regularizer += torch.norm(param, p = 1)
+        params += 1
+    regularizer /= params
+
+    return nn.BCELoss()(pred, y) + lbd * regularizer
