a3cf10870129b2d8a38569f1495331f6cbf22375
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2022-04-15 18:47:43 +0100

    Update experiments

--- example/k_experiment.py
+++ example/k_experiment.py
@@ -3,15 +3,17 @@
 # Parse command and additional parameters
 import argparse
 parser = argparse.ArgumentParser(description = 'Running k experiments of amalgamation.')
-parser.add_argument('--dataset', '-d', type = str, default = 'mimic', help = 'Dataset to analyze (child, mimic or mimic_synth).', choices = ['child', 'mimic', 'mimic_synth'])
-parser.add_argument('-k', type = int, default = 1, help = 'Number of iterations to run.')
+parser.add_argument('--dataset', '-d', type = str, default = 'mimic', help = 'Dataset to analyze (child, mimic or mimic_synth).', choices = ['child', 'mimic', 'mimic_1', 'mimic_2', 'mimic_3', 'mimic_4'])
+parser.add_argument('-k', type = int, default = 20, help = 'Number of iterations to run.')
 parser.add_argument('--selective', '-s', action='store_true', help = 'Run under selective labels.')
-
+parser.add_argument('--log', '-l', action='store_true', help = 'Run a logistic regression model (otherwise neural network).')
+parser.add_argument('-p1', default = 2.8, type = float, help = 'Threshold on center mass.')
+parser.add_argument('-p2', default = 0.95, type = float, help = 'Threshold on opposing.')
 args = parser.parse_args()
 
 print('Script running on {} for {} iterations'.format(args.dataset , args.k))
 
-params = {'layers': []} # If = [] equivalent to a simple logistic regression
+params = {'layers': [] if args.log else [100]}  # If = [] equivalent to a simple logistic regression
 l1_penalties = [0.001, 0.01, 0.1, 1, 5, 10.]
 
 rho = 0.05 # Control which point to consider from a confience point of view
@@ -23,22 +25,16 @@ import pickle as pkl
 import numpy as np
 
 if args.dataset == 'mimic':
-    pi_1 = 2.8 # Control criterion on centre mass metric
-    pi_2 = 0.95 # Control criterion on opposing metric
-    data_set = "../data/triage_semi_synthetic.csv" 
-    triage = pd.read_csv(data_set, index_col = [0, 1])
-    covariates, target, experts = triage.drop(columns = ['D', 'Y1', 'Y2', 'YC', 'acuity', 'nurse']), triage[['D', 'Y1', 'Y2', 'YC']], triage['nurse']
-
-elif args.dataset == 'mimic_synth':
-    pi_1 = 2.8 # Control criterion on centre mass metric
-    pi_2 = 0.95 # Control criterion on opposing metric
     data_set = "../data/triage_clean.csv" 
     triage = pd.read_csv(data_set, index_col = [0, 1])
     covariates, target, experts = triage.drop(columns = ['D', 'Y1', 'Y2', 'YC', 'acuity', 'nurse']), triage[['D', 'Y1', 'Y2', 'YC']], triage['nurse']
 
+elif '_' in args.dataset:
+    data_set = "../data/triage_scenario_{}.csv".format(args.dataset[args.dataset.index('_') + 1:]) 
+    triage = pd.read_csv(data_set, index_col = [0, 1])
+    covariates, target, experts = triage.drop(columns = ['D', 'Y1', 'Y2', 'YC', 'acuity', 'nurse']), triage[['D', 'Y1', 'Y2', 'YC']], triage['nurse']
+
 elif args.dataset == 'child':
-    pi_1 = 4 # Control criterion on centre mass metric
-    pi_2 = 0.8 # Control criterion on opposing metric
     with open('../../data/ChildWelfare/X_preprocess.pkl', 'rb') as handle:
         X, screener_ids, refer_ids, Y_obs, D, Y_serv,Y_sub,colnames = pkl.load(handle)
 
@@ -86,16 +82,9 @@ for k in range(args.k):
     cov_train, cov_test, tar_train, tar_test, nur_train, nur_test = train_test_split(covariates, target, experts, test_size = 0.2, random_state = k)
 
     # Train on decision
-    for l1_penalty in l1_penalties:
-        try:
-            model = BinaryMLP(**params)
-            model = model.fit(cov_train, tar_train['D'], nur_train, l1_penalty = l1_penalty, check = True)
-            break
-        except Exception as e:
-            print(e, l1_penalties.pop(0))
-            pass
-
-    Y_pred_h_test = pd.Series(model.predict(cov_test).flatten(), index = cov_test.index, name = 'Human')
+    model = BinaryMLP(**params)
+    model = model.fit(cov_train, tar_train['D'], nur_train, platt_calibration = True)
+    pred_h_test = pd.Series(model.predict(cov_test).flatten(), index = cov_test.index, name = 'Human')
 
 
     # Fold evaluation of influences
@@ -104,28 +93,44 @@ for k in range(args.k):
     
     # Amalgamation
     high_conf = (predictions > (1 - rho)) | (predictions < rho)
-    high_agr = (center_metric > pi_1) & (opposing_metric > pi_2) & high_conf
+    high_agr = (center_metric > args.p1) & (opposing_metric > args.p2) & high_conf
     high_agr_correct = ((predictions - tar_train['D']).abs() < rho) & high_agr
 
-    tar_train['Ya'] = tar_train['Y1'].astype(int)
-    tar_train['Ya'][high_agr_correct] = (1 - tau) * tar_train['Y1'][high_agr_correct] \
-                                        + tau * tar_train['D'][high_agr_correct]
+    tar_train['Ya'] = tar_train['Y1'].copy().astype(int)
+    tar_train.loc[high_agr_correct, 'Ya'] = (1 - tau) * tar_train['Y1'][high_agr_correct].copy() \
+                                                + tau * tar_train['D'][high_agr_correct].copy()
 
-    index_amalg = (tar_train['D'] | high_agr_correct) if args.selective else tar_train['D'].isin([0, 1])
+    index_amalg = ((tar_train['D'] == 1) | high_agr_correct) if args.selective else tar_train['D'].isin([0, 1])
 
 
     # Amalgamation model
     model = BinaryMLP(**params)
-    model = model.fit(cov_train[index_amalg], tar_train[index_amalg]['Ya'], nur_train[index_amalg], l1_penalty = l1_penalty)
-    Y_pred_amalg_test = pd.Series(model.predict(cov_test).flatten(), index = cov_test.index, name = 'Amalgamation')
+    model = model.fit(cov_train[index_amalg], tar_train[index_amalg]['Ya'], nur_train[index_amalg])
+    pred_amalg_test = pd.Series(model.predict(cov_test).flatten(), index = cov_test.index, name = 'Amalgamation')
 
 
     # Observed outcome
-    index_observed = tar_train['D'] if args.selective else tar_train['D'].isin([0, 1])
+    index_observed = tar_train['D'] == 1 if args.selective else tar_train['D'].isin([0, 1])
     model = BinaryMLP(**params)
-    model = model.fit(cov_train[index_observed], tar_train['Y1'][index_observed], nur_train[index_observed], l1_penalty = l1_penalty)
-    Y_pred_obs_test = pd.Series(model.predict(cov_test).flatten(), index = cov_test.index, name = 'Observed')
+    model = model.fit(cov_train[index_observed], tar_train['Y1'][index_observed], nur_train[index_observed])
+    pred_obs_test = pd.Series(model.predict(cov_test).flatten(), index = cov_test.index, name = 'Observed')
 
-    results.append(pd.concat([Y_pred_obs_test, Y_pred_amalg_test, Y_pred_h_test], axis = 1))
+
+    # Hybrid model: initialize rely on humans
+    pred_hyb_test = pred_h_test.copy().rename('Hybrid')
+
+    # Compute which test points are part of A for test set
+    predictions_test, influence_test = influence_estimate(BinaryMLP, cov_train, tar_train['D'], nur_train, cov_test, params = params, l1_penalties = [0.001, 0.01, 0.1, 1])
+    center_metric, opposing_metric = compute_agreeability(influence_test)
+    high_conf_test = (predictions_test > (1 - rho)) | (predictions_test < rho)
+    high_agr_test = (center_metric > args.p1) & (opposing_metric > args.p2) & high_conf_test
+    high_agr_correct_test = ((predictions_test - tar_test['D']).abs() < rho) & high_agr_test
+
+    # Retrain a model on non almagamation only and calibrate: Rely on observed
+    model = BinaryMLP(**params)
+    model = model.fit(cov_train[index_observed], tar_train['Y1'][index_observed], nur_train[index_observed], platt_calibration = True)
+    pred_hyb_test.loc[~high_agr_correct_test] = model.predict(cov_test.loc[~high_agr_correct_test])
+
+    results.append(pd.concat([pred_obs_test, pred_amalg_test, pred_h_test, pred_hyb_test], axis = 1))
 
 pkl.dump(results, open('../results/{}_{}.pkl'.format(args.dataset, args.k), 'wb'))
