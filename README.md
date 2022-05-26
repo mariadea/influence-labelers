35598bae8af1830e640101cad3f25ef8e36f2c06
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2022-05-26 18:52:38 +0100

    Clean repo

--- README.md
+++ README.md
@@ -1,10 +1,64 @@
-# influence-labelers
+# Leveraging Expert Consistency
+This repository implements the methodology proposed in [Leveraging Expert Consistency to Improve Algorithmic Decision Support](). The proposed approach leverages influence to estimate the consistency of multiple experts. This information is then leveraged to learn from experts when there is inferred expert consistency, and from observed labels otherwise.
 
-Git repository containing code for paper Leveraging Expert Consistency to Improve Algorithmic Decision Support https://arxiv.org/abs/2101.09648
+## How to use ?
+```python
+from nsc import NeuralSurvivalClustering
 
+# Create model for f_h
+f_h = BinaryMLP(**params)
+f_h = f_h.fit(X_train, D_train, a_train)
 
-utils-infl.py contains core functions of influence estimation
+# Estimate influence on training set
+folds, predictions, influence = influence_cv(BinaryMLP, X_train, D_train, a_train, params = params, l1_penalties = [0.001, 0.01, 0.1, 1])
 
-Results_Kruns_hybridamalg.py performs K runs of label amalgamation on child welfare data
+# Compute metrics
+center_metric, opposing_metric = compute_agreeability(influence, predictions)
 
-Kruns_hybridamalg_ViewResults.ipynb analyzes results of ^
+# Amalgamate
+high_conf = (predictions > (1 - delta)) | (predictions < delta)
+
+flat_influence = (np.abs(influence) > gamma_3).sum(0) == 0
+high_agr = (((center_metric > gamma_1) & (opposing_metric > gamma_2)) | flat_influence) & high_conf
+high_agr_correct = ((predictions - tar_train['D']).abs() < delta) & high_agr
+
+A = Y.copy()
+A[high_agr_correct] = D_train[high_agr_correct]
+index_amalg = (D == 1) | high_agr_correct # Selective labels
+
+# Train f_A
+f_A = BinaryMLP(**params)
+f_A = f_A.fit(X_train[index_amalg], A[index_amalg], a[index_amalg])
+```
+
+A full example on the MIMIC dataset is described in `example/Triage - MIMIC.ipynb` and a tutorial described how to choose the hyperparameter in `exampl/Tutorial.ipynb`
+
+## Reproduce paper's MIMIC results
+To reproduce the paper's results:
+
+0. Clone the repository with dependencies: `git clone git@github.com:XX/influence-labelers.git`.
+1. Create a conda environment with all necessary libraries `pytorch`, `pandas`, `numpy`.
+2. Download the MIMIC ED dataset and extracts data following `data/Triage - MIMIC - Preprocessing.ipynb`.
+3. Then run the comparison following `example/Triage - MIMIC.ipynb` -- `example/k_experiments.py` allows to run k iterations of this same set of experiments in command line.
+5. Analyse the results using `4. Analysis - MIMIC.ipynb`.
+
+# Setup
+## Structure
+
+### model
+This folder contains the neural network model, influence function and amalgamation functions necessary to implement the model.
+
+### example
+This folder contains the script used to run the experiment and analyze the results.
+
+### data
+This folder contains the script to extract the vital signs from MIMIC ED dataset and generate the semi synthetic labels.
+
+## Clone
+```
+git clone git@github.com:XX/influence-labelers.git
+```
+
+## Requirements
+The model relies on  `pytorch`, `pandas`, `numpy` and `sklearn`.  
+To analyze the results `matplotlib` is necessary.
--- example/k_experiment.py
+++ example/k_experiment.py
@@ -3,14 +3,14 @@
 # Parse command and additional parameters
 import argparse
 parser = argparse.ArgumentParser(description = 'Running k experiments of amalgamation.')
-parser.add_argument('--dataset', '-d', type = str, default = 'mimic', help = 'Dataset to analyze (child, mimic or mimic_synth).')
+parser.add_argument('--dataset', '-d', type = str, default = 'mimic', help = 'Dataset to analyze.')
 parser.add_argument('-k', type = int, default = 10, help = 'Number of iterations to run.')
 parser.add_argument('-s', action='store_true', help = 'Selective labels')
 parser.add_argument('--log', '-l', action='store_true', help = 'Run a logistic regression model (otherwise neural network).')
-parser.add_argument('-rho', default = 0.05, type = float, help = 'Control which point to consider from a confience point of view.')
-parser.add_argument('-p1', default = 6, type = float, help = 'Threshold on center mass.')
-parser.add_argument('-p2', default = 0.95, type = float, help = 'Threshold on opposing.')
-parser.add_argument('-p3', default = 0.002, type = float, help = 'Threshold on flat influence. Default: ignore.')
+parser.add_argument('-delta', default = 0.05, type = float, help = 'Control which point to consider from a confience point of view.')
+parser.add_argument('-gamma1', default = 6, type = float, help = 'Threshold on center mass.')
+parser.add_argument('-gamma2', default = 0.95, type = float, help = 'Threshold on opposing.')
+parser.add_argument('-gamma3', default = 0.002, type = float, help = 'Threshold on flat influence. Default: ignore.')
 args = parser.parse_args()
 
 print('Script running on {} for {} iterations'.format(args.dataset , args.k))
@@ -25,55 +25,12 @@ import pickle as pkl
 import numpy as np
 from sklearn.model_selection import GroupShuffleSplit, ShuffleSplit
 
-if args.dataset == 'mimic':
-    data_set = "../data/triage_clean.csv" 
-    triage = pd.read_csv(data_set, index_col = [0, 1])
-    splitter, groups = ShuffleSplit(n_splits = args.k, train_size = .75, random_state = 42), None
-    covariates, target, experts = triage.drop(columns = ['D', 'Y1', 'Y2', 'YC', 'nurse']), triage[['D', 'Y1', 'Y2', 'YC']], triage['nurse']
+data_set = "../data/triage_scenario_{}.csv".format(args.dataset[args.dataset.index('_') + 1:]) 
+triage = pd.read_csv(data_set, index_col = [0, 1])
+splitter, groups = ShuffleSplit(n_splits = args.k, train_size = .75, random_state = 42), None
+covariates, target, experts = triage.drop(columns = ['D', 'Y1', 'Y2', 'YC', 'nurse']), triage[['D', 'Y1', 'Y2', 'YC']], triage['nurse']
 
-    selective = args.s
-
-elif '_' in args.dataset:
-    data_set = "../data/triage_scenario_{}.csv".format(args.dataset[args.dataset.index('_') + 1:]) 
-    triage = pd.read_csv(data_set, index_col = [0, 1])
-    splitter, groups = ShuffleSplit(n_splits = args.k, train_size = .75, random_state = 42), None
-    covariates, target, experts = triage.drop(columns = ['D', 'Y1', 'Y2', 'YC', 'nurse']), triage[['D', 'Y1', 'Y2', 'YC']], triage['nurse']
-
-    selective = args.s
-
-elif args.dataset == 'child':
-    with open('../../data/ChildWelfare/X_preprocess.pkl', 'rb') as handle:
-        X, screener_ids, refer_ids, Y_obs, D, Y_serv,Y_sub,colnames = pkl.load(handle)
-
-    # Remove less than 10 observation by experts
-    drop_experts = []
-    for num in screener_ids:
-        if screener_ids.count(num) < 10:
-            drop_experts.append(num)
-
-    drop_idx = []
-    for index, elem in enumerate(screener_ids):
-        if elem in drop_experts:
-            drop_idx.append(index)
-    
-    X = np.delete(X, drop_idx, axis = 0)
-    Y_serv = np.delete(Y_serv, drop_idx, axis = 0)
-    Y_sub = np.delete(Y_sub, drop_idx, axis = 0)
-    Y_obs = np.delete(Y_obs, drop_idx, axis = 0)
-    D = np.delete(D, drop_idx, axis = 0)
-    groups = np.delete(refer_ids, drop_idx, axis = 0).flatten()
-    screener_ids = np.delete(screener_ids, drop_idx, axis = 0)
-
-    D = D.reshape((D.shape[0],))
-    Y_obs = Y_obs.reshape((Y_obs.shape[0],))
-
-    target = pd.DataFrame({'D': D, 'Y1': Y_obs, 'Y2': Y_serv, 'Y3': Y_sub})
-    target['Union'] = target.Y1 | target.Y2 | target.Y3
-    experts = pd.Series(screener_ids)
-    covariates = pd.DataFrame(X[:, :-1]) # Remove 0 columns
-    splitter = GroupShuffleSplit(n_splits = args.k, train_size = .75, random_state = 42)
-
-    selective = True
+selective = args.s
 
 # Iterate k times the algorithm
 import sys
@@ -107,10 +64,10 @@ for k, (train, test) in enumerate(splitter.split(covariates, target, groups)):
     center_metric, opposing_metric = compute_agreeability(influence, predictions)
     
     # Amalgamation
-    flat_influence = (np.abs(influence) > args.p3).sum(0) == 0
-    high_conf = (predictions > (1 - args.rho)) if args.dataset == 'child' else ((predictions > (1 - args.rho)) | (predictions < args.rho))
-    high_agr = (((center_metric > args.p1) & (opposing_metric > args.p2)) | flat_influence) & high_conf
-    high_agr_correct = (((predictions - tar_train['D']).abs() < args.rho) & high_agr)
+    flat_influence = (np.abs(influence) > args.gamma3).sum(0) == 0
+    high_conf = (predictions > (1 - args.delta)) if args.dataset == 'child' else ((predictions > (1 - args.delta)) | (predictions < args.delta))
+    high_agr = (((center_metric > args.gamma1) & (opposing_metric > args.gamma2)) | flat_influence) & high_conf
+    high_agr_correct = (((predictions - tar_train['D']).abs() < args.delta) & high_agr)
 
     ya = tar_train['Y1'].copy().astype(int)
     ya.loc[high_agr_correct] = (1 - tau) * tar_train.loc[high_agr_correct, 'Y1'].copy() \
@@ -136,10 +93,10 @@ for k, (train, test) in enumerate(splitter.split(covariates, target, groups)):
     # Compute which test points are part of A for test set
     predictions_test, influence_test = influence_estimate(BinaryMLP, cov_train, tar_train['D'], nur_train, cov_test, params = params, l1_penalties = l1_penalties, groups = None if groups is None else groups[train])
     center_metric, opposing_metric = compute_agreeability(influence_test, predictions_test)
-    flat_influence_test = (np.abs(influence_test) > args.p3).sum(0) == 0
-    high_conf_test = (predictions_test > (1 - args.rho)) if args.dataset == 'child' else ((predictions_test > (1 - args.rho)) | (predictions_test < args.rho))
-    high_agr_test = (((center_metric > args.p1) & (opposing_metric > args.p2)) | flat_influence_test) & high_conf_test
-    high_agr_correct_test = ((predictions_test - tar_test['D']).abs() < args.rho) & high_agr_test
+    flat_influence_test = (np.abs(influence_test) > args.gamma3).sum(0) == 0
+    high_conf_test = (predictions_test > (1 - args.delta)) if args.dataset == 'child' else ((predictions_test > (1 - args.delta)) | (predictions_test < args.delta))
+    high_agr_test = (((center_metric > args.gamma1) & (opposing_metric > args.gamma2)) | flat_influence_test) & high_conf_test
+    high_agr_correct_test = ((predictions_test - tar_test['D']).abs() < args.delta) & high_agr_test
 
     # Retrain a model on non almagamation only and calibrate: Rely on observed
     model = BinaryMLP(**params)
@@ -154,4 +111,4 @@ for k, (train, test) in enumerate(splitter.split(covariates, target, groups)):
     results.append(pd.concat([pred_obs_test, pred_amalg_test, pred_h_test, pred_hyb_test, pred_defer], axis = 1))
     amalgamation.append([high_agr_correct.mean()])
 
-    pkl.dump({'results': results, 'amalgamation': amalgamation}, open('../results/{}_{}_rho={}_p1={}_p2={}_p3={}{}.pkl'.format(args.dataset, 'log' if args.log else 'mlp', args.rho, args.p1, args.p2, args.p3, '_selective' if selective else ''), 'wb'))
+    pkl.dump({'results': results, 'amalgamation': amalgamation}, open('../results/{}_{}_delta={}_gamma1={}_gamma2={}_gamma3={}{}.pkl'.format(args.dataset, 'log' if args.log else 'mlp', args.delta, args.gamma1, args.gamma2, args.gamma3, '_selective' if selective else ''), 'wb'))
