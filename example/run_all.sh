b3dab7167f73f3dbae195561b1f94758ee0411fd
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2024-09-13 04:12:29 -0400

    Add script run all

---
+++ example/run_all.sh
@@ -0,0 +1,12 @@
+conda activate
+
+# Run all experiments for MIMIC
+python3.11 k_experiment.py -d mimic_1
+python3.11 k_experiment.py -d mimic_2
+python3.11 k_experiment.py -d mimic_3_0.3
+python3.11 k_experiment.py -d mimic_3_0.5
+python3.11 k_experiment.py -d mimic_3_0.7
+python3.11 k_experiment.py -d mimic_4
+python3.11 k_experiment.py -d mimic_5
+python3.11 k_experiment.py -d mimic_6
+python3.11 k_experiment.py -d mimic_7
